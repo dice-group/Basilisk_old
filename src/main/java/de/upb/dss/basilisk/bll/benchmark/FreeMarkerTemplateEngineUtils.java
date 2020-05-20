2c2d0342b0c604bce26b8ceb0cdc80decc1d8cb6
rsethi13 <rahulsethi90@gmail.com>
2020-05-20 23:18:20 +0200

    Created template to create a script to load the testdata into the virtuoso dynamically.

--- src/main/java/de/upb/dss/basilisk/bll/benchmark/FreeMarkerTemplateEngineUtils.java
+++ src/main/java/de/upb/dss/basilisk/bll/benchmark/FreeMarkerTemplateEngineUtils.java
@@ -79,4 +79,65 @@ public class FreeMarkerTemplateEngineUtils {
         }
         return 0;
     }
+
+    /**
+     * This method creates the script to load test data into virtuoso tripe store.
+     *
+     * @return Status code.
+     */
+    public static int setLoadingScript(String testDataset, String containerId, String port) {
+
+        Logger logger = new LoggerUtils().getLogger(new ApplicationPropertiesUtils().getLogFilePath(),
+                "FreeMarker Utils");
+        //Get the freemarker configuration
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_29);
+
+        try {
+            //Set up the free marker configuration, with template loading class and path.
+            cfg.setClassForTemplateLoading(FreeMarkerTemplateEngineUtils.class, "/");
+            cfg.setDefaultEncoding("UTF-8");
+
+            //Get the Iguana configuration template.
+            Template template = cfg.getTemplate("loadTestDataIntoVirtuoso.ftl");
+
+            //Port number and query file to insert into benchmark template
+            Map<String, Object> templateData = new HashMap<>();
+            templateData.put("port", port);
+            templateData.put("testDataset", testDataset);
+            templateData.put("containerId", containerId);
+
+            //Write port number and query file in to template
+            StringWriter out = new StringWriter();
+            template.process(templateData, out);
+            out.flush();
+
+            //Dump that configuration into a configuration file called benchmark.config
+            String fileSeparator = System.getProperty("file.separator");
+
+            File scriptFile = new File("loadTestData.sh");
+
+            if (scriptFile.exists()) {
+                if (!scriptFile.delete()) {
+                    System.err.println("Could not generate config file");
+                    return -1;
+                }
+            }
+
+            if (scriptFile.createNewFile()) {
+                scriptFile.setExecutable(true,false);
+                FileOutputStream fos = new FileOutputStream(scriptFile);
+                fos.write(out.toString().getBytes());
+                fos.flush();
+                fos.close();
+                logger.info(scriptFile + " File Created");
+            } else {
+                System.out.println("Something went wrong while creating the file " + scriptFile);
+                logger.info("Something went wrong while creating the file " + scriptFile);
+                return -1;
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        return 0;
+    }
 }
