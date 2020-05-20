520341e84f783e7515752ff0b8d2987bb1bbe7c1
zeeshanullah <zeeshanrao.engg@gmail.com>
2020-05-20 19:49:48 +0200

    added d3 c3 and bootstrap to new angular project

---
+++ app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import * as c3 from 'c3';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+}
