2933d446c79f3f141b719fd1467fa7d66ccb49df
Ranjith K <ranjith.masthikatte@gmail.com>
2020-06-08 23:56:31 +0200

    Added operation available for admin to authorize the user.

---
+++ src/main/java/de/upb/dss/basilisk/security/OPERATIONTYPE.java
@@ -0,0 +1,12 @@
+package de.upb.dss.basilisk.security;
+
+/**
+ * This ENUM indicates the type of operation that admin can perform for authorizing the user.
+ *
+ * @author Ranjith Krishnamurthy
+ */
+public enum OPERATIONTYPE {
+    APPROVE,
+    PENDING,
+    REJECT
+}
